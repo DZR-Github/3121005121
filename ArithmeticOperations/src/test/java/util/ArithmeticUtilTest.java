b3ce8472c0dfe665502bc3c65988f15eed826aa9
zrDeng <2014358827@qq.com>
2023-09-27 19:11:05 +0800

    四则运算

---
+++ ArithmeticOperations/src/test/java/util/ArithmeticUtilTest.java
@@ -0,0 +1,22 @@
+package util;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ArithmeticUtilTest {
+
+    @Test
+    public void buildArithmetic() {
+        ArithmeticUtil.buildArithmetic(10,10);
+    }
+
+    @Test
+    public void checkExercise() {
+        String path = "E:\\大三上\\软件工程\\3121005121\\3121005121\\ArithmeticOperations\\target\\test-classes\\";
+        ArithmeticUtil.checkExercise(path+"e.txt",path+"a.txt");
+    }
+}
